2ab2cf4eeffa86a12ab0ceb30440f0a50a95f8e4
priti-29 <113707711+priti-29@users.noreply.github.com>
2025-04-04 12:55:12 +0530

    changes

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -3,7 +3,7 @@ import User from '../models/User.js';
 
 //API Controller Function to manage clerk user with db
 
-export const clerkWebhooks = async (requestAnimationFrame,res)=>{
+export const clerkWebhooks = async (req,res)=>{
     try{
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
         await whook.verify(JSON.stringify(req.body),{
